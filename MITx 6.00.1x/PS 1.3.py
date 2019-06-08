d8fedc93a26c70fd26109eb2150e535d4bceac01
Andrew Yin <51426389+andrewyinn@users.noreply.github.com>
2019-06-08 18:09:37 -0500

    Update PS 1.3.py

--- MITx 6.00.1x/PS 1.3.py
+++ MITx 6.00.1x/PS 1.3.py
@@ -14,25 +14,12 @@ Solution by: Andrew Yin
 
 cur_str = ""
 long_str = ""
-alphabetized = True
 
-for i in range(0, len(s)):
-    if i < len(s) -1 and s[i] <= s[i+1]:
-        cur_str += s[i]
-        alphabetized = True
-    elif i < len(s) -1 and alphabetized == True and s[i] >= s[i+1]:
-        cur_str += s[i]
-        alphabetized = False
-        if len(cur_str) > len(long_str):
-            long_str = cur_str
-        cur_str = ""
-    elif i == len(s)-1 and s[i] >= s[i-1]:
-        cur_str += s[i]
-    else:
-        cur_str = ""
-        alphabetized = False
-    
+for i in range(0,len(s)):
+    cur_str += s[i]
     if len(cur_str) > len(long_str):
         long_str = cur_str
-    
+    if i < len(s)-1 and s[i] > s[i+1]:
+        cur_str = ""
+        
 print("Longest substring in alphabetical order is: " + long_str)
