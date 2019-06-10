cc75ed972a6919ac24ac787b3d8b69c761fe27a6
Andrew Yin <51426389+andrew-yin@users.noreply.github.com>
2019-06-10 11:04:26 -0500

    Update Hofstadter's Q Sequence.py

--- Hofstadter's Q Sequence.py
+++ Hofstadter's Q Sequence.py
@@ -1,8 +1,5 @@
-# -*- coding: utf-8 -*-
 """
-Created on Mon Jun 10 10:30:03 2019
-
-@author: Andrew Yin
+By: Andrew Yin
 """
 
 import matplotlib.pyplot as plt
@@ -17,7 +14,15 @@ def hofstadters_q_seq(n):
 
 num = 500
 
-plt.xlabel('n')
-plt.ylabel('Q(n)')
+plt.subplot(1,2,1)
+plt.xlabel("n")
+plt.ylabel("Q(n)")
 plt.plot(range(1,num+1), hofstadters_q_seq(num))
+
+plt.subplot(1,2,2)
+plt.xlabel("n")
+plt.ylabel("Q(n) - n/2")
+plt.plot(range(1,num+1), [m - n for m,n in zip(hofstadters_q_seq(num),[x / 2 for x in range(1,num+1)])])
+         
+plt.tight_layout()
 plt.show()
